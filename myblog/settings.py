ce7f86a980b29ed3451e502db196e9f4c26d48d7
dayeong <da1599@naver.com>
2022-07-31 14:08:05 +0900

    0731

--- myblog/settings.py
+++ myblog/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL='/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+##STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
